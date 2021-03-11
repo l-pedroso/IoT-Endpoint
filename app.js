0349e60f5bc93d189767950938008d4ac73898c9
lpedroso <lpedroso@margirius.com.br>
2021-03-11 14:06:47 -0300

    app folder changed

---
+++ app.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./src/routes/index')
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
+
+
+//=== 1 - CREATE APP
+// Creating express app and configuring middleware needed for authentication
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+//=== 2 - SET UP DATABASE
+//Configure mongoose's promise to global promise
+mongoose.promise = global.Promise;
+mongoose.connect(process.env.MONGO_CONN_URL, { useNewUrlParser: true , useCreateIndex: true}).then(() => {
+    console.log('MongoDB --  database connection established successfully!');
+}).catch(err => {
+    console.log('Connection to database error' + err);
+});
+    
+mongoose.connection.on('error', err => {
+    console.log("MongoDB connection error. Please make sure MongoDB is running. " + err);
+    process.exit();
+});
+
+
+//==== 3 - ROUTES INITIALIZATION
+app.use('/api/v1', routes);
+
+//=== 4 - START SERVER
+//app.listen(process.env.PORT || 3000, () => console.log('Server running on http://localhost:'+process.env.PORT+'/'));
+
+app.listen(process.env.PORT);
